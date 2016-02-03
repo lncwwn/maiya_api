c5dc6d900a75daf052efaaf8cb45fbfe8a000fbf
linianchao <linianchao@meituan.com>
2016-02-03 13:50:31 +0800

    upd: refine code

--- server.js
+++ server.js
@@ -1,36 +1,13 @@
 /**
- * Server entry.
+ * Provides server instance.
  *
  * @author victor li
- * @date 2016/02/01
+ * @date 2016/02/03
  */
 
 'use strict';
 
 const Hapi = require('hapi');
 
-const APP_SETTING = require('./settings/app_setting.json');
-
-const server = new Hapi.Server();
-
-server.connection({
-    host: 'localhost',
-    port: APP_SETTING.port
-});
-
-server.route({
-    method: 'GET',
-    path: '/test',
-    handler: (request, reply) => {
-        return reply('Hello, World');
-    }
-});
-
-server.start((err)=> {
-    if (err) {
-        throw err;
-    }
-    console.log('Server start at:', server.info.uri);
-});
-
+module.exports = new Hapi.Server();
 
